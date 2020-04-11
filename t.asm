a0dc02c3d727633d03578c90e37a5f618e27e151
sentimentGod <sentimental1243@gmail.com>
2020-04-11 18:22:39 +0300

    refresh

---
+++ t.asm
@@ -0,0 +1,37 @@
+.386
+Data 	Segment use16
+	Vb		db	1h
+	Vd		dd	0d5h
+	String1		db	"Assembler"
+	Times1		EQU 	10
+	Times2		EQU 	0
+Data 	ends
+
+Code	Segment use16
+ASSUME CS:CODE, DS:DATA
+label1:
+	jz	lend
+	add	vb[ebp*8],56h
+	mov	eax, Vd
+	dec 	eax
+	push	100
+	mov	esi, 1
+	test	eax,FS:Vd[esi*2] 
+	bts	Vd[esi*4], eax
+	add	String1[esi*2], 1
+	IF Times1
+		M1 dd 7ah
+		test eax, M1
+	ENDIF
+	IF Times2
+		mov	edi, String1
+	ENDIF
+lend:
+	stosd
+	cmp	eax, esi
+	test 	bh,vd[esi*8]
+	jz label1
+	add al,bl
+	and eax,tbx
+Code 	ends
+END
